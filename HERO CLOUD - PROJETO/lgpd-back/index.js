63d7c6e9c31829b2700122d7fe63b32d31cb58cf
Lucas Proença <lucasproencasilva@gmail.com>
2024-03-02 12:04:49 -0300

    Adicionando integração frontend com backend na branch nova para backend

---
+++ HERO CLOUD - PROJETO/lgpd-back/index.js
@@ -0,0 +1,31 @@
+import { express } from "express";
+import { pkg } from "body-parser";
+import { router } from "./routes/router.js"
+
+import sequelize from "./utils/database.js";
+import association from "./utils/Associations.js";
+
+import cors from "cors";
+
+const app = express();
+const { json, urlendcoded } = pkg;
+
+app.use(json());
+app.use(urlendcoded({ extended: true}));
+app.use("/", router);
+app.use(cors());
+
+(async () => {
+    try {
+        association.associations();
+        await sequelize.sync();
+        app.listen(3000, function () {
+            console.log("Listening from 3000");
+        });
+        
+    } catch (error) {
+        console.log(error);    
+    }
+})();
+
+app.use("/", router);
